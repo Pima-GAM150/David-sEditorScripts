fbef07f4f0eea15fd6a0555cabe608ca64be4567
David Keane <zilonkeane@gmail.com>
2019-04-07 13:46:46 -0700

    Copied over work done in class and tidied up some code
    In Class I
    ♦ Created the code to rotate all selected objects
    ♦ Created the dropdown menu to select the six different degrees you can rotate by
    ♦ Created toggle to rotate by X Axis
    ♦ Created toggle to rotate by Y Axis
    ♦ Created toggle to rotate by Z Axis
    ♦ Created toggle to rotate in the opposite direction
    ♦ Created button to reset Rotation
    ♦ Learned how to make the code without a hundred if statements so it looks cleaner

--- Assets/EditorScript.cs
+++ Assets/EditorScript.cs
@@ -5,6 +5,22 @@ public class EditorScript : EditorWindow
 {
     public Vector3 scale;
 
+    public int index = 0;
+    bool xBoolChecked = false;
+    bool yBoolChecked = false;
+    bool zBoolChecked = false;
+    bool inverseRotate = false;
+    int invertRotation = 1;
+    bool reset;
+    public int Rotate;
+
+    public string[] options = new string[] { "15 Degrees", "30 Degrees", "45 Degrees", "60 Degrees", "75 Degrees", "90 Degrees" };
+    float[] optionValues = new float[] { 15f, 30f, 45f, 60f, 75f, 90f };
+    public string[] scaleSetoptions = new string[] { "0.25", "0.5", "0.75", "1.5", "2", "5" };
+    float[] scaleSetOptionValues = new float[] { 0.25f, 0.5f, 0.75f, 1.5f, 2f, 5f, };
+    public string[] scaleMultiplyoptions = new string[] { "0.25", "0.5", "0.75", "1.5", "2", "5" };
+    float[] scaleMultiplyOptionValues = new float[] { 0.25f, 0.5f, 0.75f, 1.5f, 2f, 5f, };
+
     [MenuItem("Window/Object Manipulator")]
     public static void ShowWindow()
     {
@@ -13,10 +29,36 @@ public class EditorScript : EditorWindow
 
     private void OnGUI()
     {
+        xBoolChecked = EditorGUILayout.Toggle("Rotate X", xBoolChecked);
+        yBoolChecked = EditorGUILayout.Toggle("Rotate Y", yBoolChecked);
+        zBoolChecked = EditorGUILayout.Toggle("Rotate Z", zBoolChecked);
+        inverseRotate = EditorGUILayout.Toggle("Inverse Rotation", inverseRotate);
+        if (inverseRotate)
+        {
+            invertRotation = -1;
+        }
+        else
+        {
+            invertRotation = 1;
+        }
+        index = EditorGUILayout.Popup(index, options);
+        if (GUILayout.Button("Rotate"))
+        {
+            RotationFunction();
+        }
+        reset = GUILayout.Button("Reset Rotation");
+        if (reset)
+        {
+            foreach (GameObject obj in Selection.gameObjects)
+            {
+                obj.GetComponent<Transform>().rotation *= Quaternion.identity;
+            }
+        }
+
         GUILayout.Label("Scale the Selected Object", EditorStyles.boldLabel);
         scale = EditorGUILayout.Vector3Field("Scale", scale);
-        EditorGUILayout.DropdownButton(new GUIContent("Scale Presets"), FocusType.Passive);
-        EditorGUILayout.DropdownButton(new GUIContent("Multiplicative Scale"), FocusType.Passive);
+        EditorGUILayout.Popup(index, scaleSetoptions);
+        EditorGUILayout.Popup(index, scaleMultiplyoptions);
         EditorGUILayout.DropdownButton(new GUIContent("Setting Scale"), FocusType.Passive);
 
         if (GUILayout.Button("Reset Scale"))
@@ -97,4 +139,18 @@ public class EditorScript : EditorWindow
             obj.GetComponent<Transform>().localScale = scale;
         }
     }
-}
+    void RotationFunction()
+    {
+        Vector3 rotationToApply = new Vector3();
+        if (xBoolChecked) rotationToApply.x = optionValues[index];
+        if (yBoolChecked) rotationToApply.y = optionValues[index];
+        if (zBoolChecked) rotationToApply.z = optionValues[index];
+
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            obj.GetComponent<Transform>().rotation = Quaternion.Euler(invertRotation * rotationToApply.x, invertRotation * rotationToApply.y, invertRotation * rotationToApply.z);
+        }
+
+    }
+
+    }
